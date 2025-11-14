d92f08802bc3b48f1b49320fece54e1fea9bba37
GigichOK <denihsanov2016@gmail.com>
2025-11-14 12:37:31 +0500

    dark tema

--- script.js
+++ script.js
@@ -38,3 +38,41 @@ applyBtn.addEventListener("click", () => {
 
   block.style.display = "none";
 });
+const themeToggleBtn = document.getElementById("themeToggle");
+const themeToggleBtn_img = document.querySelector("#themeToggle img");
+
+themeToggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (themeToggleBtn_img.getAttribute("src") === "moon.svg") {
+    themeToggleBtn_img.setAttribute("src", "sun.svg");
+  } else {
+    themeToggleBtn_img.setAttribute("src", "moon.svg");
+  }
+});
+// const text_note = document.querySelector(".your-note").value;
+// const label = document.createElement("label");
+// const input = document.createElement("input");
+// const span_fake_cb = document.createElement("span");
+// const span_text = document.createElement("span");
+
+// label.classList.add("item");
+// input.classList.add("cb");
+// input.setAttribute("type", "checkbox");
+
+// span_fake_cb.classList.add("fake-cb");
+// span_fake_cb.setAttribute("aria-hidden", "true");
+
+// span_text.classList.add("text");
+// span_text.innerText = text_note;
+
+// label.appendChild(input);
+// label.appendChild(span_fake_cb);
+// label.appendChild(span_text);
+
+// document.querySelector(".tasks").appendChild(label);
+
+// document.querySelector(".block").style.display = "none";
+
+
+
